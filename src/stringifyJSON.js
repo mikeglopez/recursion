a81737de3fbdb77d8bab51abd943f7b5f304b0c9
Mike Lopez <mikeglopez@me.com>
2018-10-23 21:06:01 -0500

    "Implement array recursion case"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -19,12 +19,20 @@ var stringifyJSON = function(obj) {
 	else if (typeof obj === 'string') {
 		return '"' + obj + '"';
 	}
+	else if (Array.isArray(obj)) {
+  		var arr = [];
+  		if (obj.length > 0) {
+  			obj.forEach(function(item) {
+  				arr.push(stringifyJSON(item));
+  			});
+  		}
+  		return '[' + arr + ']';
+  	}
 	else if (typeof obj === 'object') {
 		return;
 	}
-  	
 };
 
-
+console.log(typeof stringifyJSON([1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999]));
 
 
