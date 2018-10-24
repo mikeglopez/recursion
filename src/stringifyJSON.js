a85a54f998aa7cb7f4e9798e60ad8425432e5f4b
Mike Lopez <mikeglopez@me.com>
2018-10-23 23:55:34 -0500

    "Implement object recursion case"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,35 +4,36 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-	if (obj === null) {
+	if (typeof obj === 'function' || typeof obj === 'undefined') {
+		return '';
+	}
+	else if (obj === null) {
 		return 'null';
 	}
-	else if (typeof obj === 'function' || typeof obj === 'undefined') {
-		return;
-	}
-	else if (typeof obj === 'boolean') {
-		return '' + obj;
-	}
-	else if (typeof obj === 'number') {
+	else if (typeof obj === 'boolean' || typeof obj === 'number') {
 		return '' + obj;
 	}
 	else if (typeof obj === 'string') {
 		return '"' + obj + '"';
 	}
 	else if (Array.isArray(obj)) {
-  		var arr = [];
+  		var outputArr = [];
   		if (obj.length > 0) {
   			obj.forEach(function(item) {
-  				arr.push(stringifyJSON(item));
+  				outputArr.push(stringifyJSON(item));
   			});
   		}
-  		return '[' + arr + ']';
+  		return '[' + outputArr + ']';
   	}
 	else if (typeof obj === 'object') {
-		return;
+		var outputObj = [];
+		if (Object.keys(obj).length > 0) {
+			for (var key in obj) {
+				if(stringifyJSON(obj[key]).length > 0) {
+					outputObj.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+				}
+			}
+		}
+		return '{' + outputObj.join(',') + '}';
 	}
 };
-
-console.log(typeof stringifyJSON([1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999]));
-
-
