6e3af0f2687d351dd21ae9d704e3858d4f295156
Mike Lopez <mikeglopez@me.com>
2018-10-25 09:45:57 -0500

    "Implement recursive containsClass function"

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+	var outputArr = [];
+	function containsClass(container) {
+		var containerList = container.classList;
+		if (containerList && containerList.contains(className)) {
+			outputArr.push(container);
+		}
+		if (container.childNodes) {
+			container.childNodes.forEach(function(item) {
+				containsClass(item);
+			});
+		}
+	}
+	containsClass(document.body);
+	return outputArr;
 };
